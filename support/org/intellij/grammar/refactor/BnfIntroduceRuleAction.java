f88386af02810e352c40369c57541caa4074a6ca
Gregory Shrago <gregory.shrago@jetbrains.com>
2012-06-08 17:53:34 +0400

    hide Introduce Rule in non BNF files

--- support/org/intellij/grammar/refactor/BnfIntroduceRuleAction.java
+++ support/org/intellij/grammar/refactor/BnfIntroduceRuleAction.java
@@ -18,8 +18,10 @@ package org.intellij.grammar.refactor;
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.RefactoringActionHandler;
 import com.intellij.refactoring.actions.BasePlatformRefactoringAction;
+import org.intellij.grammar.psi.BnfFile;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -36,6 +38,11 @@ public class BnfIntroduceRuleAction extends BasePlatformRefactoringAction {
 
   protected boolean isAvailableInEditorOnly() {
     return true;
+  }
+
+  @Override
+  protected boolean isAvailableForFile(PsiFile file) {
+    return file instanceof BnfFile;
   }
 
   protected boolean isEnabledOnElements(PsiElement[] elements) {

d88baaec9ac929862d18b24dea236f62054d2912
Gregory Shrago <gregory.shrago@jetbrains.com>
2013-04-17 18:28:03 +0400

    errorReported propagation when rollback expected fix

--- support/org/intellij/grammar/parser/GeneratedParserUtilBase.java
+++ support/org/intellij/grammar/parser/GeneratedParserUtilBase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011-2013 Gregory Shrago
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.intellij.grammar.parser;
 
 import com.intellij.lang.*;
@@ -304,7 +320,8 @@ public class GeneratedParserUtilBase {
       state.FRAMES.recycle(frame);
       return result;
     }
-    if (!result && !pinned && initialOffset == frame.offset && state.lastExpectedVariantOffset == frame.offset &&
+    boolean willFail = !result && !pinned;
+    if (willFail && initialOffset == frame.offset && state.lastExpectedVariantOffset == frame.offset &&
         frame.name != null && state.variants.size() - frame.variantCount > 1) {
       state.clearVariants(true, frame.variantCount);
       addVariantInner(state, initialOffset, frame.name);
@@ -368,8 +385,8 @@ public class GeneratedParserUtilBase {
         reportError(builder_, state, frame, false, false);
       }
     }
-    // propagate errorReportedAt up the stack to avoid duplicate reporting
-    Frame prevFrame = state.frameStack.isEmpty() ? null : state.frameStack.getLast();
+    // propagate errorReportedAt up the stack to avoid duplicate reporting except marker.rollback case
+    Frame prevFrame = willFail && sectionType == _SECTION_GENERAL_ || state.frameStack.isEmpty() ? null : state.frameStack.getLast();
     if (prevFrame != null && prevFrame.errorReportedAt < frame.errorReportedAt) prevFrame.errorReportedAt = frame.errorReportedAt;
     state.FRAMES.recycle(frame);
     return result;

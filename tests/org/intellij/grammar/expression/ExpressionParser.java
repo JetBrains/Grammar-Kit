ec60f11f0afe23f793e075ec11b744e5e9241231
Gregory Shrago <gregory.shrago@jetbrains.com>
2013-12-04 18:05:49 +0400

    GPUB: improve zero-length tokens support & clarify recovered elements placement

--- tests/org/intellij/grammar/expression/ExpressionParser.java
+++ tests/org/intellij/grammar/expression/ExpressionParser.java
@@ -95,7 +95,7 @@ public class ExpressionParser implements PsiParser {
   };
 
   /* ********************************************************** */
-  // '(' [ expr  (',' expr) * ] ')'
+  // '(' [ !')' expr  (',' expr) * ] ')'
   public static boolean arg_list(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "arg_list")) return false;
     boolean result_ = false;
@@ -109,41 +109,52 @@ public class ExpressionParser implements PsiParser {
     return result_ || pinned_;
   }
 
-  // [ expr  (',' expr) * ]
+  // [ !')' expr  (',' expr) * ]
   private static boolean arg_list_1(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "arg_list_1")) return false;
     arg_list_1_0(builder_, level_ + 1);
     return true;
   }
 
-  // expr  (',' expr) *
+  // !')' expr  (',' expr) *
   private static boolean arg_list_1_0(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "arg_list_1_0")) return false;
     boolean result_ = false;
     boolean pinned_ = false;
     Marker marker_ = enter_section_(builder_, level_, _NONE_, null);
-    result_ = expr(builder_, level_ + 1, -1);
+    result_ = arg_list_1_0_0(builder_, level_ + 1);
     pinned_ = result_; // pin = 1
-    result_ = result_ && arg_list_1_0_1(builder_, level_ + 1);
+    result_ = result_ && report_error_(builder_, expr(builder_, level_ + 1, -1));
+    result_ = pinned_ && arg_list_1_0_2(builder_, level_ + 1) && result_;
     exit_section_(builder_, level_, marker_, null, result_, pinned_, null);
     return result_ || pinned_;
   }
 
+  // !')'
+  private static boolean arg_list_1_0_0(PsiBuilder builder_, int level_) {
+    if (!recursion_guard_(builder_, level_, "arg_list_1_0_0")) return false;
+    boolean result_ = false;
+    Marker marker_ = enter_section_(builder_, level_, _NOT_, null);
+    result_ = !consumeToken(builder_, ")");
+    exit_section_(builder_, level_, marker_, null, result_, false, null);
+    return result_;
+  }
+
   // (',' expr) *
-  private static boolean arg_list_1_0_1(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "arg_list_1_0_1")) return false;
+  private static boolean arg_list_1_0_2(PsiBuilder builder_, int level_) {
+    if (!recursion_guard_(builder_, level_, "arg_list_1_0_2")) return false;
     int pos_ = current_position_(builder_);
     while (true) {
-      if (!arg_list_1_0_1_0(builder_, level_ + 1)) break;
-      if (!empty_element_parsed_guard_(builder_, "arg_list_1_0_1", pos_)) break;
+      if (!arg_list_1_0_2_0(builder_, level_ + 1)) break;
+      if (!empty_element_parsed_guard_(builder_, "arg_list_1_0_2", pos_)) break;
       pos_ = current_position_(builder_);
     }
     return true;
   }
 
   // ',' expr
-  private static boolean arg_list_1_0_1_0(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "arg_list_1_0_1_0")) return false;
+  private static boolean arg_list_1_0_2_0(PsiBuilder builder_, int level_) {
+    if (!recursion_guard_(builder_, level_, "arg_list_1_0_2_0")) return false;
     boolean result_ = false;
     boolean pinned_ = false;
     Marker marker_ = enter_section_(builder_, level_, _NONE_, null);
@@ -155,38 +166,42 @@ public class ExpressionParser implements PsiParser {
   }
 
   /* ********************************************************** */
-  // expr
+  // expr ';'
   static boolean element(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "element")) return false;
     boolean result_ = false;
-    Marker marker_ = enter_section_(builder_, level_, _NONE_, null);
-    result_ = expr(builder_, level_ + 1, -1);
-    exit_section_(builder_, level_, marker_, null, result_, false, element_recover_parser_);
-    return result_;
-  }
-
-  /* ********************************************************** */
-  // element ';'
-  static boolean element_and_separator(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "element_and_separator")) return false;
-    boolean result_ = false;
     boolean pinned_ = false;
     Marker marker_ = enter_section_(builder_, level_, _NONE_, null);
-    result_ = element(builder_, level_ + 1);
+    result_ = expr(builder_, level_ + 1, -1);
     pinned_ = result_; // pin = 1
     result_ = result_ && consumeToken(builder_, ";");
-    exit_section_(builder_, level_, marker_, null, result_, pinned_, null);
+    exit_section_(builder_, level_, marker_, null, result_, pinned_, element_recover_parser_);
     return result_ || pinned_;
   }
 
   /* ********************************************************** */
-  // !';'
+  // !('(' | '+' | '-' | 'multiply' | id | number)
   static boolean element_recover(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "element_recover")) return false;
     boolean result_ = false;
     Marker marker_ = enter_section_(builder_, level_, _NOT_, null);
-    result_ = !consumeToken(builder_, ";");
+    result_ = !element_recover_0(builder_, level_ + 1);
     exit_section_(builder_, level_, marker_, null, result_, false, null);
+    return result_;
+  }
+
+  // '(' | '+' | '-' | 'multiply' | id | number
+  private static boolean element_recover_0(PsiBuilder builder_, int level_) {
+    if (!recursion_guard_(builder_, level_, "element_recover_0")) return false;
+    boolean result_ = false;
+    Marker marker_ = enter_section_(builder_);
+    result_ = consumeToken(builder_, "(");
+    if (!result_) result_ = consumeToken(builder_, "+");
+    if (!result_) result_ = consumeToken(builder_, "-");
+    if (!result_) result_ = consumeToken(builder_, "multiply");
+    if (!result_) result_ = consumeToken(builder_, ID);
+    if (!result_) result_ = consumeToken(builder_, NUMBER);
+    exit_section_(builder_, marker_, null, result_);
     return result_;
   }
 
@@ -203,12 +218,12 @@ public class ExpressionParser implements PsiParser {
   }
 
   /* ********************************************************** */
-  // element_and_separator *
+  // element *
   static boolean root(PsiBuilder builder_, int level_) {
     if (!recursion_guard_(builder_, level_, "root")) return false;
     int pos_ = current_position_(builder_);
     while (true) {
-      if (!element_and_separator(builder_, level_ + 1)) break;
+      if (!element(builder_, level_ + 1)) break;
       if (!empty_element_parsed_guard_(builder_, "root", pos_)) break;
       pos_ = current_position_(builder_);
     }

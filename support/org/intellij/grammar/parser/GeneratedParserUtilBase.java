4e8427b321bf831b938397e74e9eaba98aeea399
Gregory Shrago <gregory.shrago@jetbrains.com>
2014-04-27 18:13:29 +0400

    move getTokenType() up so white-spaces are skipped before rawTokenIndex() call

--- support/org/intellij/grammar/parser/GeneratedParserUtilBase.java
+++ support/org/intellij/grammar/parser/GeneratedParserUtilBase.java
@@ -200,6 +200,7 @@ public class GeneratedParserUtilBase {
     if (state.completionState != null) return true;
     boolean track = !state.suppressErrors && state.predicateCount < 2 && state.predicateSign;
     if (!track) return nextTokenIsFast(builder_, tokens);
+    IElementType tokenType = builder_.getTokenType();
     if (StringUtil.isNotEmpty(frameName)) {
       addVariantInner(state, builder_.rawTokenIndex(), frameName);
     }
@@ -208,7 +209,6 @@ public class GeneratedParserUtilBase {
         addVariant(builder_, state, token);
       }
     }
-    IElementType tokenType = builder_.getTokenType();
     if (tokenType == null) return false;
     for (IElementType token : tokens) {
       if (tokenType == token) return true;

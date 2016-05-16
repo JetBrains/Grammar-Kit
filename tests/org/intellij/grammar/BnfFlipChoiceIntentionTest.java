dac20d41111ff915314fe620fb5601e7cc2d24bb
Gregory Shrago <gregory.shrago@jetbrains.com>
2016-05-17 00:09:09 +0300

    wrap in write-command action

--- tests/org/intellij/grammar/BnfFlipChoiceIntentionTest.java
+++ tests/org/intellij/grammar/BnfFlipChoiceIntentionTest.java
@@ -1,6 +1,8 @@
 package org.intellij.grammar;
 
 import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 import org.intellij.grammar.intention.BnfFlipChoiceIntention;
 
@@ -21,11 +23,16 @@ public class BnfFlipChoiceIntentionTest extends LightPlatformCodeInsightFixtureT
   public void testMultipleChoice() throws Exception {doTest("rule ::= a | b | c <caret>| d","rule ::= a | b | d | c");}
   public void testComplexCase() throws Exception {doTest("rule ::= a | b | c c c <caret>| d [d d]","rule ::= a | b | d [d d] | c c c");}
 
-  private void doTest(/*@Language("BNF")*/ String text, /*@Language("BNF")*/ String expected) throws IOException {
+  private void doTest(/*@Language("BNF")*/ String text, /*@Language("BNF")*/ final String expected) throws IOException {
     myFixture.configureByText("a.bnf", text);
-    IntentionAction action = new BnfFlipChoiceIntention();
+    final IntentionAction action = new BnfFlipChoiceIntention();
     assertTrue("intention not available", action.isAvailable(getProject(), myFixture.getEditor(), myFixture.getFile()));
-    action.invoke(getProject(), myFixture.getEditor(), myFixture.getFile());
+    new WriteCommandAction.Simple(getProject()) {
+      @Override
+      protected void run() throws Throwable {
+        action.invoke(getProject(), myFixture.getEditor(), myFixture.getFile());
+      }
+    }.execute();
     assertSameLines(expected, myFixture.getFile().getText());
   }
 }

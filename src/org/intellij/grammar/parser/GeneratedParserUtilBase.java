206c18a0c6bf16760e6ae662944fd27945089785
Gregory Shrago <gregory.shrago@jetbrains.com>
2016-09-16 21:09:58 +0300

    GPUB: endless loop guard in parseAsTree()

--- src/org/intellij/grammar/parser/GeneratedParserUtilBase.java
+++ src/org/intellij/grammar/parser/GeneratedParserUtilBase.java
@@ -154,7 +154,7 @@ public class GeneratedParserUtilBase {
 
   public static boolean recursion_guard_(PsiBuilder builder, int level, String funcName) {
     if (level > MAX_RECURSION_LEVEL) {
-      builder.error("Maximum recursion level (" + MAX_RECURSION_LEVEL + ") reached in '" + funcName + "'");
+      builder.mark().error("Maximum recursion level (" + MAX_RECURSION_LEVEL + ") reached in '" + funcName + "'");
       return false;
     }
     return true;
@@ -162,6 +162,7 @@ public class GeneratedParserUtilBase {
 
   public static boolean empty_element_parsed_guard_(PsiBuilder builder, String funcName, int pos) {
     if (pos == current_position_(builder)) {
+      // sometimes this is a correct situation, therefore no explicit marker
       builder.error("Empty element parsed in '" + funcName + "' at offset " + builder.getCurrentOffset());
       return false;
     }
@@ -559,7 +560,7 @@ public class GeneratedParserUtilBase {
     if (state.hooks == null) return;
     PsiBuilder.Marker marker = elementType == null ? null : (PsiBuilder.Marker)builder.getLatestDoneMarker();
     if (elementType != null && marker == null) {
-      builder.error("No expected done marker at offset " + builder.getCurrentOffset());
+      builder.mark().error("No expected done marker at offset " + builder.getCurrentOffset());
     }
     while (state.hooks != null && state.hooks.level >= state.level) {
       if (state.hooks.level == state.level) {
@@ -779,10 +780,8 @@ public class GeneratedParserUtilBase {
     boolean notEmpty = isNotEmpty(expectedText);
     if (!(force || notEmpty || advance)) return false;
 
-    String gotText = builder.eof() ? "unexpected end of file" :
-                           notEmpty ? "got '" + first(notNullize(builder.getTokenText(), "null"), MAX_ERROR_TOKEN_TEXT, true) + "'" :
-                           "'" + first(notNullize(builder.getTokenText(), "null"), MAX_ERROR_TOKEN_TEXT, true) +"' unexpected";
-    String message = expectedText + gotText;
+    String actual = "'" + first(notNullize(builder.getTokenText(), "null"), MAX_ERROR_TOKEN_TEXT, true) + "'";
+    String message = expectedText + (builder.eof() ? "unexpected end of file" : notEmpty ? "got " + actual : actual + " unexpected");
     if (advance) {
       PsiBuilder.Marker mark = builder.mark();
       builder.advanceLexer();
@@ -1183,6 +1182,7 @@ public class GeneratedParserUtilBase {
         parenList.add(Pair.create(((PsiBuilder.Marker)doneMarker).precede(), (PsiBuilder.Marker)null));
       }
     }
+    int c = current_position_(builder);
     while (true) {
       final IElementType tokenType = builder.getTokenType();
       if (checkParens && (tokenType == state.braces[0].getLeftBraceType() || tokenType == state.braces[0].getRightBraceType() && !parenList.isEmpty())) {
@@ -1214,7 +1214,7 @@ public class GeneratedParserUtilBase {
           marker = builder.mark();
           marker.setCustomEdgeTokenBinders(WhitespacesBinders.GREEDY_LEFT_BINDER, null);
         }
-        final boolean result = (!parenList.isEmpty() || eatMoreCondition.parse(builder, level + 1)) && parser.parse(builder, level + 1);
+        boolean result = (!parenList.isEmpty() || eatMoreCondition.parse(builder, level + 1)) && parser.parse(builder, level + 1);
         if (result) {
           tokenCount++;
           totalCount++;
@@ -1224,15 +1224,17 @@ public class GeneratedParserUtilBase {
         }
       }
 
-      if (tokenCount >= MAX_CHILDREN_IN_TREE && marker != null) {
+      if (tokenCount >= MAX_CHILDREN_IN_TREE) {
         marker.done(chunkType);
         siblingList.addFirst(Pair.create(marker, 1));
         checkSiblingsRunnable.run();
         marker = null;
         tokenCount = 0;
       }
+      if (!empty_element_parsed_guard_(builder, "parseAsTree", c)) break;
+      c = current_position_(builder);
     }
-    marker.drop();
+    if (marker != null) marker.drop();
     for (Pair<PsiBuilder.Marker, PsiBuilder.Marker> pair : parenList) {
       pair.first.drop();
     }

bae216ddb5b6f5c277f07859bd23f05b1e2e9e03
Gregory Shrago <gregory.shrago@jetbrains.com>
2016-11-04 06:26:27 +0300

    cleanup: make lines shorter

--- tests/org/intellij/grammar/BnfCompletionTest.java
+++ tests/org/intellij/grammar/BnfCompletionTest.java
@@ -2,49 +2,57 @@ package org.intellij.grammar;
 
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.testFramework.UsefulTestCase;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
+import com.intellij.util.containers.ContainerUtil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+
+import static org.intellij.grammar.BnfCompletionTest.CheckType.*;
 
 /**
  * @author gregsh
  */
 public class BnfCompletionTest extends JavaCodeInsightFixtureTestCase {
-  enum CheckType { EQUALS, INCLUDES, EXCLUDES }
 
-  public void testKeywords1() throws Throwable { doTestVariants("", CompletionType.BASIC, 1, CheckType.INCLUDES, "private", "meta"); }
-  public void testKeywords2() throws Throwable { doTestVariants("{a=b}<caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "private", "meta"); }
-  public void testKeywords21() throws Throwable { doTestVariants("{a=b}<caret>", CompletionType.BASIC, 1, CheckType.EXCLUDES, "pin"); }
-  public void testKeywords3() throws Throwable { doTestVariants("{a=b} private rule::= <caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "private", "meta"); }
-  public void testKeywords4() throws Throwable { doTestVariants("{a=b} <caret>rule::=", CompletionType.BASIC, 1, CheckType.INCLUDES, "private", "meta"); }
-  public void testKeywords5() throws Throwable { doTestVariants("{a=b} pri<caret>rule::=", CompletionType.BASIC, 1, CheckType.INCLUDES, "private"); }
-  public void testKeywords6() throws Throwable { doTestVariants("rule::= pri<caret>other ::=", CompletionType.BASIC, 1, CheckType.INCLUDES, "private"); }
-  public void testKeywords7() throws Throwable { doTestVariants("pr<caret>rule::=", CompletionType.BASIC, 1, CheckType.INCLUDES, "private"); }
-  public void testKeywords8() throws Throwable { doTestVariants("rule::=\npr<caret>rule::=", CompletionType.BASIC, 1, CheckType.INCLUDES, "private"); }
-  public void testKeywordsNotInComments() throws Throwable { doTestVariants("root ::= //rule ::= p<caret>", CompletionType.BASIC, 1, CheckType.EXCLUDES, "public", "private"); }
+  public void testKeywords1() { variants("", INCLUDES, "private", "meta"); }
+  public void testKeywords2() { variants("{a=b}<caret>", INCLUDES, "private", "meta"); }
+  public void testKeywords21() { variants("{a=b}<caret>", EXCLUDES, "pin"); }
+  public void testKeywords3() { variants("{a=b} private rule::= <caret>", INCLUDES, "private", "meta"); }
+  public void testKeywords4() { variants("{a=b} <caret>rule::=", INCLUDES, "private", "meta"); }
+  public void testKeywords5() { variants("{a=b} pri<caret>rule::=", INCLUDES, "private"); }
+  public void testKeywords6() { variants("rule::= pri<caret>other ::=", INCLUDES, "private"); }
+  public void testKeywords7() { variants("pr<caret>rule::=", INCLUDES, "private"); }
+  public void testKeywords8() { variants("rule::=\npr<caret>rule::=", INCLUDES, "private"); }
+  public void testKeywordsNotInComments() { variants("root ::= //rule ::= p<caret>", EXCLUDES, "public", "private"); }
 
-  public void testAttr1() throws Throwable { doTestVariants("{<caret>}", CompletionType.BASIC, 1, CheckType.INCLUDES, "pin"); }
-  public void testAttr2() throws Throwable { doTestVariants("{<caret>}", CompletionType.BASIC, 1, CheckType.INCLUDES, "pin"); }
-  public void testAttr3() throws Throwable { doTestVariants("{pin=1 rec<caret>overUntil=abc}", CompletionType.BASIC, 1, CheckType.INCLUDES, "recoverWhile"); }
-  public void testAttr4() throws Throwable { doTestVariants("a::= {name=\"A\" p<caret>=\"\"}", CompletionType.BASIC, 1, CheckType.INCLUDES, "pin", "elementType"); }
-  public void testAttr5() throws Throwable { doTestVariants("expr ::= {name=\"A\" e<caret>xtends=\"\"}", CompletionType.BASIC, 1, CheckType.EXCLUDES, "expr"); }
-  public void testAttr6() throws Throwable { doTestVariants("expr ::= {<caret>e= }", CompletionType.BASIC, 1, CheckType.EXCLUDES, "expr"); }
+  public void testAttr1() { variants("{<caret>}", INCLUDES, "pin"); }
+  public void testAttr2() { variants("{<caret>}", INCLUDES, "pin"); }
+  public void testAttr3() { variants("{pin=1 rec<caret>overUntil=abc}", INCLUDES, "recoverWhile"); }
+  public void testAttr4() { variants("a::= {name=\"A\" p<caret>=\"\"}", INCLUDES, "pin", "elementType"); }
+  public void testAttr5() { variants("expr ::= {name=\"A\" e<caret>xtends=\"\"}", EXCLUDES, "expr"); }
+  public void testAttr6() { variants("expr ::= {<caret>e= }", EXCLUDES, "expr"); }
 
-  public void testToken1() throws Throwable { doTestVariants("a ::= TOK TOKEN b ::= T<caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "TOKEN"); }
-  public void testToken2() throws Throwable { doTestVariants("{tokens=[TOK TOKEN]} a ::= T<caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "TOKEN"); }
+  public void testToken1() { variants("a ::= TOK TOKEN b ::= T<caret>", INCLUDES, "TOKEN"); }
+  public void testToken2() { variants("{tokens=[TOK TOKEN]} a ::= T<caret>", INCLUDES, "TOKEN"); }
 
-  public void testRule1() throws Throwable { doTestVariants("rule::= <caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "rule"); }
-  public void testRule2() throws Throwable { doTestTextAfter("<with space>::= ws<caret>", CompletionType.BASIC, 1, "<with space>::= <with space>"); }
+  public void testRule1() { variants("rule::= <caret>", INCLUDES, "rule"); }
+  public void testRule2() { textAfter("<with space>::= ws<caret>", CompletionType.BASIC, "<with space>::= <with space>");
+  }
 
-  public void testExternalMethod1() throws Throwable { doTestVariants(initUtil() + "external rule::= <caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "eofX", "eofY"); }
-  public void testExternalMethod2() throws Throwable { doTestVariants(initUtil() + "external rule::= abc <caret>", CompletionType.BASIC, 1, CheckType.EXCLUDES, "eofX", "eofY"); }
-  public void testExternalMethod3() throws Throwable { doTestVariants(initUtil() + "root ::= external rule ::= <caret>", CompletionType.BASIC, 1, CheckType.EXCLUDES, "root"); }
-  public void testExternalMethod4() throws Throwable { doTestVariants(initUtil() + "root ::= external rule ::= abc <caret>", CompletionType.BASIC, 1, CheckType.INCLUDES, "root"); }
-  public void testExternalMethod5() throws Throwable { doTestVariants(initUtil() + "root ::= meta metaR ::= rule ::= <<<caret> >>", CompletionType.BASIC, 1, CheckType.INCLUDES, "eofX", "eofY", "metaR"); }
-  public void testExternalMethod6() throws Throwable { doTestVariants(initUtil() + "root ::= rule ::= <<<caret> >>", CompletionType.BASIC, 1, CheckType.EXCLUDES, "root"); }
+  public void testExternalMethod1() { variants(initUtil() + "external rule::= <caret>",
+                                               INCLUDES, "eofX", "eofY"); }
+  public void testExternalMethod2() { variants(initUtil() + "external rule::= abc <caret>",
+                                               EXCLUDES, "eofX", "eofY"); }
+  public void testExternalMethod3() { variants(initUtil() + "root ::= external rule ::= <caret>",
+                                               EXCLUDES, "root"); }
+  public void testExternalMethod4() { variants(initUtil() + "root ::= external rule ::= abc <caret>",
+                                               INCLUDES, "root"); }
+  public void testExternalMethod5() { variants(initUtil() + "root ::= meta metaR ::= rule ::= <<<caret> >>",
+                                               INCLUDES, "eofX", "eofY", "metaR"); }
+  public void testExternalMethod6() { variants(initUtil() + "root ::= rule ::= <<<caret> >>",
+                                               EXCLUDES, "root"); }
 
   private String initUtil() {
     myFixture.addClass("public class X { public static boolean eofX(com.intellij.lang.PsiBuilder b, int l) { } }");
@@ -52,32 +60,34 @@ public class BnfCompletionTest extends JavaCodeInsightFixtureTestCase {
     return "{ parserUtilClass='Y' } ";
   }
 
-  protected void doTestVariants(String txt, CompletionType type, int count, CheckType checkType, String... variants) throws Throwable {
+  protected void variants(String txt, CheckType checkType, String... variants) {
     myFixture.configureByText("a.bnf", txt);
-    doTestVariantsInner(type, count, checkType, variants);
+    doVariantsTestInner(myFixture, CompletionType.BASIC, checkType, variants);
   }
 
-  protected void doTestTextAfter(String txt, CompletionType type, int count, String textAfter) throws Throwable {
+  protected void textAfter(String txt, CompletionType type, String textAfter) {
     myFixture.configureByText("a.bnf", txt);
-    myFixture.complete(type, count);
+    myFixture.complete(type, 1);
     assertEquals(textAfter, myFixture.getEditor().getDocument().getText());
   }
 
-  protected void doTestVariantsInner(CompletionType type, int count, CheckType checkType, String... variants) throws Throwable {
-    myFixture.complete(type, count);
-    List<String> stringList = myFixture.getLookupElementStrings();
+  public enum CheckType {EQUALS, INCLUDES, EXCLUDES}
+
+  public static void doVariantsTestInner(CodeInsightTestFixture fixture, CompletionType type, CheckType checkType, String... variants) {
+    fixture.complete(type, 1);
+    List<String> stringList = fixture.getLookupElementStrings();
     assertNotNull(stringList);
-    Collection<String> varList = new ArrayList<String>(Arrays.asList(variants));
-    if (checkType == CheckType.EQUALS) {
+    Collection<String> varList = ContainerUtil.newArrayList(variants);
+    if (checkType == EQUALS) {
       UsefulTestCase.assertSameElements(stringList, variants);
     }
-    else if (checkType == CheckType.INCLUDES) {
+    else if (checkType == INCLUDES) {
       varList.removeAll(stringList);
-      assertTrue("Missing variants: " + varList, varList.isEmpty());
+      assertTrue("Missing: " + varList + " in " + stringList, varList.isEmpty());
     }
-    else if (checkType == CheckType.EXCLUDES) {
+    else if (checkType == EXCLUDES) {
       varList.retainAll(stringList);
-      assertTrue("Unexpected variants: "+varList, varList.isEmpty());
+      assertTrue("Unexpected: " + varList + " in " + stringList, varList.isEmpty());
     }
   }
 

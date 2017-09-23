fa46bd33ea5d1e01e94aaafab2983c8f6ea724fd
Gregory Shrago <gregory.shrago@jetbrains.com>
2017-09-23 03:45:29 +0300

    cleanup

--- src/org/intellij/grammar/psi/impl/BnfStringManipulator.java
+++ src/org/intellij/grammar/psi/impl/BnfStringManipulator.java
@@ -18,25 +18,26 @@ package org.intellij.grammar.psi.impl;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
-import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author gregsh
  */
 public class BnfStringManipulator extends AbstractElementManipulator<BnfStringImpl> {
   @Override
-  public BnfStringImpl handleContentChange(BnfStringImpl psi, TextRange range, String newContent) throws IncorrectOperationException {
-    final String oldText = psi.getText();
-    final String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
+  public BnfStringImpl handleContentChange(@NotNull BnfStringImpl psi, @NotNull TextRange range, String newContent) {
+    String oldText = psi.getText();
+    String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
     return psi.updateText(newText);
   }
 
+  @NotNull
   @Override
-  public TextRange getRangeInElement(final BnfStringImpl element) {
+  public TextRange getRangeInElement(@NotNull BnfStringImpl element) {
     return getStringTokenRange(element);
   }
 
-  public static TextRange getStringTokenRange(final BnfStringImpl element) {
+  public static TextRange getStringTokenRange(BnfStringImpl element) {
     return TextRange.from(1, element.getTextLength()-2);
   }
 }

476527ddec43051a8003ab5f468f610f0c2f0188
Boris Krylov <boris.krylov@jetbrains.com>
2024-04-13 05:03:05 +0300

    [fleet]Lexer generator action for Fleet

---
+++ src/org/intellij/grammar/actions/BnfGenerateFleetLexerAction.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package org.intellij.grammar.actions;
+
+import com.intellij.openapi.util.text.StringUtil;
+import org.apache.velocity.VelocityContext;
+import org.intellij.grammar.KnownAttribute;
+import org.intellij.grammar.generator.BnfConstants;
+import org.intellij.grammar.psi.BnfFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static org.intellij.grammar.generator.ParserGeneratorUtil.getRootAttribute;
+
+public class BnfGenerateFleetLexerAction extends BnfGenerateLexerAction {
+
+
+  private static final String FLEET_LEXER_FLEX_TEMPLATE = "/templates/fleet.lexer.flex.template";
+
+  @Override
+  protected String getLexerFlexTemplate() { return FLEET_LEXER_FLEX_TEMPLATE; }
+
+  @Override
+  protected void putPackageName(@NotNull VelocityContext context, BnfFile bnfFile, @Nullable String packageName) {
+    context.put("packageName", BnfConstants.FLEET_NAMESPACE_PREFIX + StringUtil.notNullize(packageName, StringUtil.getPackageName(getRootAttribute(bnfFile, KnownAttribute.PARSER_CLASS))));
+  }
+
+  @Override
+  protected void putTypeHolderClass(@NotNull VelocityContext context, BnfFile bnfFile) {
+    context.put("typesClass", BnfConstants.FLEET_NAMESPACE_PREFIX + getRootAttribute(bnfFile, KnownAttribute.ELEMENT_TYPE_HOLDER_CLASS));
+  }
+
+}

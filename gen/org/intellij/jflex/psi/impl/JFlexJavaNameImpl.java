23b701711cbb72ccedc5e1aa3a88667af1cafcd2
Yann Cébron <yann.cebron@jetbrains.com>
2024-05-08 17:43:59 +0200

    regen JFlex (only changed license header)

--- gen/org/intellij/jflex/psi/impl/JFlexJavaNameImpl.java
+++ gen/org/intellij/jflex/psi/impl/JFlexJavaNameImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2011-present JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package org.intellij.jflex.psi.impl;
 

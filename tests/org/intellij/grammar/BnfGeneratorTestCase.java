ff721dc45d4ab00c9acb9967ce9a79e641bcd095
Boris Krylov <boris.krylov@jetbrains.com>
2024-05-14 02:01:49 +0300

    Revert "[fleet] Fixing test compilation"
    This reverts commit f511e14cef7abe483d973799e5e7d1496244e316.

--- tests/org/intellij/grammar/BnfGeneratorTestCase.java
+++ tests/org/intellij/grammar/BnfGeneratorTestCase.java
@@ -5,7 +5,6 @@ import com.intellij.concurrency.JobLauncherImpl;
 import com.intellij.lang.LanguageASTFactory;
 import com.intellij.lang.LanguageBraceMatching;
 import com.intellij.lang.injection.InjectedLanguageManager;
-import com.intellij.openapi.application.impl.ReadActionCacheImpl;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.DumbServiceImpl;
 import com.intellij.psi.PsiFileFactory;
@@ -13,6 +12,8 @@ import com.intellij.psi.PsiReferenceService;
 import com.intellij.psi.PsiReferenceServiceImpl;
 import com.intellij.psi.impl.PsiFileFactoryImpl;
 import com.intellij.psi.impl.source.resolve.ResolveCache;
+import com.intellij.psi.impl.source.tree.injected.EditorWindowTracker;
+import com.intellij.psi.impl.source.tree.injected.EditorWindowTrackerImpl;
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageManagerImpl;
 
 /**
@@ -28,9 +29,8 @@ public class BnfGeneratorTestCase extends AbstractParsingTestCase {
     super.setUp();
     getApplication().registerService(JobLauncher.class, JobLauncherImpl.class);
     getApplication().registerService(PsiReferenceService.class, PsiReferenceServiceImpl.class);
-    getApplication().registerService(com.intellij.psi.util.ReadActionCache.class, ReadActionCacheImpl.class);
-    getProject().registerService(DumbService.class, new DumbServiceImpl(
-      getProject(), kotlinx.coroutines.CoroutineScopeKt.CoroutineScope(kotlin.coroutines.EmptyCoroutineContext.INSTANCE)));
+    getApplication().registerService(EditorWindowTracker.class, EditorWindowTrackerImpl.class);
+    getProject().registerService(DumbService.class, DumbServiceImpl.class);
     getProject().registerService(InjectedLanguageManager.class, InjectedLanguageManagerImpl.class);
     getProject().registerService(PsiFileFactory.class, PsiFileFactoryImpl.class);
     getProject().registerService(ResolveCache.class, ResolveCache.class);

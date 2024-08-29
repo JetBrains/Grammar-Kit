761e451d0a28fc6f435d3ff9f4e3b124150bd346
Boris Krylov <boris.krylov@jetbrains.com>
2024-08-29 14:17:06 +0300

    [fleet] adjust test data for testExprParser

--- testData/fleet/ExprParser.PSI.expected.java
+++ testData/fleet/ExprParser.PSI.expected.java
@@ -1,7 +1,3 @@
-/*
- * Copyright 2011-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
-
 // ---- ExpressionTypes.java -----------------
 //header.txt
 package fleet.org.intellij.grammar.expression;

311842f8d4f2598e17e5f86a38fabeb31db99419
Yann Cébron <yann.cebron@jetbrains.com>
2024-10-09 11:14:33 +0200

    LightPsi: suppress warnings

--- src/org/intellij/grammar/LightPsi.java
+++ src/org/intellij/grammar/LightPsi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2011-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package org.intellij.grammar;
@@ -194,6 +194,7 @@ public class LightPsi {
 
   public static class Init {
 
+    @SuppressWarnings("UnstableApiUsage")
     public static void initExtensions(MockApplication application, @NotNull MockProject project) {
       ExtensionsAreaImpl ra = application.getExtensionArea();
       ra.registerExtensionPoint("com.intellij.referencesSearch", "com.intellij.util.QueryExecutor", INTERFACE, false);

f67258db9423f7106ebe2b30bd39c80800834f37
Boris Krylov <boris.krylov@jetbrains.com>
2025-04-13 02:23:37 +0300

    Updating testdata with new imports lists

--- testData/syntax/PsiGen.expected.java
+++ testData/syntax/PsiGen.expected.java
@@ -7,13 +7,12 @@ import com.intellij.platform.syntax.parser.SyntaxTreeBuilder.Marker;
 import static generated.GeneratedSyntaxElementTypes.*;
 import static PsiGenUtil.*;
 import static com.intellij.platform.syntax.runtime.SyntaxGeneratedParserRuntimeKt.*;
-import static com.intellij.platform.syntax.parser.ProductionResult.*;
-import static com.intellij.platform.syntax.parser.ProductionResultKt.prepareProduction;
-import kotlin.jvm.functions.Function2;
-import kotlin.Unit;
 import com.intellij.platform.syntax.SyntaxElementType;
 import com.intellij.platform.syntax.parser.ProductionResult;
 import com.intellij.platform.syntax.SyntaxElementTypeSet;
+import static com.intellij.platform.syntax.parser.ProductionResultKt.prepareProduction;
+import kotlin.jvm.functions.Function2;
+import kotlin.Unit;
 
 @SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
 public class PsiGen {
@@ -359,10 +358,6 @@ import com.intellij.platform.syntax.parser.SyntaxTreeBuilder.Marker;
 import static generated.GeneratedSyntaxElementTypes.*;
 import static PsiGenUtil.*;
 import static com.intellij.platform.syntax.runtime.SyntaxGeneratedParserRuntimeKt.*;
-import static com.intellij.platform.syntax.parser.ProductionResult.*;
-import static com.intellij.platform.syntax.parser.ProductionResultKt.prepareProduction;
-import kotlin.jvm.functions.Function2;
-import kotlin.Unit;
 import static PsiGen.*;
 
 @SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
@@ -586,10 +581,6 @@ import com.intellij.platform.syntax.parser.SyntaxTreeBuilder.Marker;
 import static generated.GeneratedSyntaxElementTypes.*;
 import static PsiGenUtil.*;
 import static com.intellij.platform.syntax.runtime.SyntaxGeneratedParserRuntimeKt.*;
-import static com.intellij.platform.syntax.parser.ProductionResult.*;
-import static com.intellij.platform.syntax.parser.ProductionResultKt.prepareProduction;
-import kotlin.jvm.functions.Function2;
-import kotlin.Unit;
 import static PsiGen.*;
 
 @SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})

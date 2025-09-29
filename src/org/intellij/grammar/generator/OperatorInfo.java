f48a761ddd70804dccfa923b3516eeea745cc24a
Boris Krylov <boris.krylov@jetbrains.com>
2025-09-30 00:04:13 +0300

    Convert OperatorInfo into a record class

--- src/org/intellij/grammar/generator/OperatorInfo.java
+++ src/org/intellij/grammar/generator/OperatorInfo.java
@@ -6,36 +6,18 @@ package org.intellij.grammar.generator;
 
 import org.intellij.grammar.psi.BnfExpression;
 import org.intellij.grammar.psi.BnfRule;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
-public class OperatorInfo {
-  public final BnfRule rule;
-  public final OperatorType type;
-  public final BnfExpression operator;
-  public final BnfExpression tail; // null for postfix
-  public final BnfRule arg1;
-  public final BnfRule arg2;
-
+/**
+ * @param tail null for postfix
+ */
+public record OperatorInfo(BnfRule rule, OperatorType type, BnfExpression operator, BnfExpression tail, BnfRule arg1, BnfRule arg2) {
   public OperatorInfo(BnfRule rule, OperatorType type, BnfExpression operator, BnfExpression tail) {
     this(rule, type, operator, tail, null, null);
   }
 
-  public OperatorInfo(BnfRule rule, OperatorType type, BnfExpression operator, BnfExpression tail,
-                      @Nullable BnfRule arg1,
-                      @Nullable BnfRule arg2) {
-    if (operator == null) {
-      throw new AssertionError(rule + ": operator must not be null");
-    }
-    this.rule = rule;
-    this.type = type;
-    this.operator = operator;
-    this.tail = tail;
-    this.arg1 = arg1;
-    this.arg2 = arg2;
-  }
-
   @Override
-  public String toString() {
+  public @NotNull String toString() {
     return type + "(" + rule.getName() + ")";
   }
 }

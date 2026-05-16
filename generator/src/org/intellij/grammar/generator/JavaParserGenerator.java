5b7b657a35868743b67ef7bac358c5c87e091687
Max Medvedev <maxim.medvedev@jetbrains.com>
2026-05-16 11:33:53 +0200

    remove unnecessary braces

--- generator/src/org/intellij/grammar/generator/JavaParserGenerator.java
+++ generator/src/org/intellij/grammar/generator/JavaParserGenerator.java
@@ -207,9 +207,7 @@ public final class JavaParserGenerator extends Generator {
 
   @Override
   public void generate() throws IOException {
-    {
-      generateParser();
-    }
+    generateParser();
     generatePsi();
   }
   
